87d77f93e1d60567edc86add1bb2c184fb99f8fb
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-09-17 16:03:05 +0200

    Dodanie aplikacji do ladowania jsona

---
+++ src/Basics/ProcessJsonConsoleApp/Models/TruckUnload.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessJsonConsoleApp.Models
+{
+    class TruckUnload
+    {
+        public ProcessReport Process { get; set; }
+    }
+
+    class ProcessReport
+    {
+        public string ProcessType { get; set; } // Wlasciwosc (Property)
+        public Header Header { get; set; }
+    }
+
+    class Header
+    {
+        public string ControllerDpName { get; set; }
+        public ReportTypes ReportType { get; set; }
+    }
+
+    enum ReportTypes : byte
+    {
+        Etanol = 1,
+        Ester = 2,
+        Addin = 3,
+    }
+}
---
+++ src/Basics/ProcessJsonConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using ProcessJsonConsoleApp.Models;
+
+Console.WriteLine("Hello, Json!");
+
+
+string json = File.ReadAllText("Assets/process.json");
+
+Console.WriteLine(json);
+
+TruckUnload truckUnload1 = new TruckUnload();
+truckUnload1.Process = new ProcessReport();
+truckUnload1.Process.Header = new Header();
+truckUnload1.Process.ProcessType = "TU1";
+
+TruckUnload truckUnload2 = new TruckUnload();
+
+
+
+Header header = new Header();
+header.ReportType = ReportTypes.Etanol;
+
+if (header.ReportType == ReportTypes.Etanol)
+{
+    Console.WriteLine((byte) header.ReportType);
+}
